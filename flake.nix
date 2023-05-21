63697c5319ba2abcf33815db7eb2c746f5295c0f
Benjamin Hajdukiewicz <70604257+katanallama@users.noreply.github.com>
2023-05-21 06:33:45 -0600

    fixup flake

--- flake.nix
+++ flake.nix
@@ -1,45 +1,6 @@
-# {
-#   inputs = {
-#     mvn2nix.url = "github:fzakaria/mvn2nix";
-#     utils.url = "github:numtide/flake-utils";
-#   };
-
-#   outputs = { nixpkgs, mvn2nix, utils, ... }:
-#     let
-#       overlay = final: prev: {
-#         projectile-simulation-suite = final.callPackage ./default.nix { };
-#       };
-
-#       pkgsForSystem = system:
-#         import nixpkgs {
-#           overlays = [ mvn2nix.overlay overlay ];
-#           inherit system;
-#         };
-
-#     in utils.lib.eachSystem utils.lib.defaultSystems (system: rec {
-#       legacyPackages = pkgsForSystem system;
-#       packages = utils.lib.flattenTree {
-#         inherit (legacyPackages) projectile-simulation-suite;
-#       };
-
-#       defaultPackage = legacyPackages.projectile-simulation-suite;
-
-#       devShell = legacyPackages.mkShellNoCC {
-#         name = "java";
-#         buildInputs = [
-#           legacyPackages.jdk
-#           legacyPackages.maven
-#           legacyPackages.jdt-language-server
-#           packages.projectile-simulation-suite
-#         ];
-#       };
-#     });
-# }
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    # nixpkgs.url = "github:NixOS/nixpkgs/22.11";
-    # nixpkgs.url = "path:/home/bh/projects/nixpkgs"; # for testing
     mvn2nix.url = "github:fzakaria/mvn2nix";
     utils.url = "github:numtide/flake-utils";
   };
@@ -58,17 +19,17 @@
         };
     in utils.lib.eachSystem utils.lib.defaultSystems (system: rec {
       legacyPackages = pkgsForSystem system;
-      # packages = utils.lib.flattenTree {
-      #   inherit (legacyPackages) projectile-simulation-suite;
-      # };
-      # defaultPackage = legacyPackages.projectile-simulation-suite;
+      packages = utils.lib.flattenTree {
+        inherit (legacyPackages) projectile-simulation-suite;
+      };
+      defaultPackage = legacyPackages.projectile-simulation-suite;
       devShell = legacyPackages.mkShellNoCC {
         name = "java";
         buildInputs = [
           legacyPackages.jdk17
           legacyPackages.maven
           legacyPackages.jdt-language-server
-          # packages.projectile-simulation-suite
+          packages.projectile-simulation-suite
         ];
         shellHook = ''
           export JAVA_HOME=${legacyPackages.jdk17}/
