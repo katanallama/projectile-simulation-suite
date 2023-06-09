b6907d6115f8caea4f99112f13040defab5435c3
Benjamin Hajdukiewicz <70604257+katanallama@users.noreply.github.com>
2023-06-09 08:44:32 -0600

    added basic handlers test templates

---
+++ src/test/java/com/pss/handlers/GetProjectileGravityTest.java
@@ -0,0 +1,22 @@
+package com.pss.handlers;
+
+import com.pss.interfaces.*;
+
+import javax.vecmath.Vector3d;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class GetProjectileGravityTest {
+    @Test
+    void getProjectileGravity() {
+        IGetConfiguration configHandler = new StubGetConfiguration();
+        IGetProjectile projectileHandler = new GetProjectile(configHandler);
+        GetProjectileGravity gravityHandler = new GetProjectileGravity(configHandler, projectileHandler);
+
+        Vector3d gravity = gravityHandler.getProjectileGravity();
+
+        assertEquals(0, gravity.getX());
+        assertEquals(0, gravity.getY());
+        assertEquals(-98.10000000000001, gravity.getZ());
+    }
+}
---
+++ src/test/java/com/pss/handlers/StubGetProjectileDragTest.java
@@ -0,0 +1,18 @@
+package com.pss.handlers;
+
+import javax.vecmath.Vector3d;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class StubGetProjectileDragTest {
+    @Test
+    void getProjectileDrag() {
+        StubGetProjectileDrag dragHandler = new StubGetProjectileDrag();
+
+        Vector3d drag = dragHandler.getProjectileDrag();
+
+        assertEquals(0, drag.getX());
+        assertEquals(0, drag.getY());
+        assertEquals(0, drag.getZ());
+    }
+}
