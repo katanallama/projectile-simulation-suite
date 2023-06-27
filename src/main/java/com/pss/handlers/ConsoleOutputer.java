ae3bed1a6d55512430c5aae5ea0f558fea21a28a
Benjamin Hajdukiewicz <70604257+katanallama@users.noreply.github.com>
2023-06-26 20:13:49 -0600

    format output to 1 decimal place

--- src/main/java/com/pss/handlers/ConsoleOutputer.java
+++ src/main/java/com/pss/handlers/ConsoleOutputer.java
@@ -1,11 +1,21 @@
 package com.pss.handlers;
+
 import com.pss.interfaces.IOutputResults;
+
 import javax.vecmath.Vector3d;
 
-public class ConsoleOutputer implements IOutputResults{
+public class ConsoleOutputer implements IOutputResults {
     public void outputResults(Vector3d[] results) {
-        for (int i = 0; i < results.length; i++) {
-            System.out.println(results[i].toString());
+        for (Vector3d vector : results) {
+            System.out.println(formatVector(vector));
         }
+    }
+
+    private String formatVector(Vector3d vector) {
+        return "(" + formatDouble(vector.x) + ", " + formatDouble(vector.y) + ", " + formatDouble(vector.z) + ")";
+    }
+
+    private String formatDouble(double value) {
+        return String.format("%.1f", value);
     }
 }
