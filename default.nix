a6606330ffc92ad687436ec0dcc2fcfd1d7ae8e5
Benjamin Hajdukiewicz <70604257+katanallama@users.noreply.github.com>
2023-07-01 12:45:16 -0600

    switch to buildMavenPackage

--- default.nix
+++ default.nix
@@ -1,38 +1,38 @@
 {
-  lib,
-  stdenv,
-  callPackage,
+  jdk17,
   makeWrapper,
   maven,
-  jdk17,
   nix-gitignore,
 }: let
-  repository = callPackage ./.build-maven-repo.nix {};
+  version = "0.1";
+  pname = "pss";
+  name = "${pname}-${version}";
+  src = nix-gitignore.gitignoreSource ["*.nix"] ./.;
+
+  mavenJdk17 = maven.override {
+    jdk = jdk17;
+  };
+
 in
-  stdenv.mkDerivation rec {
-    pname = "pss";
-    version = "0.1";
-    name = "${pname}-${version}";
-    src = nix-gitignore.gitignoreSource ["*.nix"] ./.;
+  mavenJdk17.buildMavenPackage rec{
+    inherit pname name src version;
+
+    mvnHash = "sha256-mBF36zHv1BRf3E4uD2uaGBT1EyydDrbkSmT+v2wAfYg=";
 
     nativeBuildInputs = [jdk17 maven makeWrapper];
-
-    buildPhase = ''
-      echo "Building with maven repository ${repository}"
-      mvn package --offline -Dmaven.repo.local=${repository}
-    '';
 
     installPhase = ''
       # create the bin directory
       mkdir -p $out/bin
 
       # create a symbolic link for the lib directory
-      ln -s ${repository} $out/lib
+      ln -s $fetchedMavenDeps/.m2 $out/lib
 
       # copy out the JAR
       # Maven already setup the classpath to use m2 repository layout
       # with the prefix of lib/
       cp target/${name}.jar $out/
+
 
       # create a wrapper that will automatically set the classpath
       # this should be the paths from the dependency derivation
