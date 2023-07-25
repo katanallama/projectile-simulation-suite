c078fcaf5d683e45e35a03b5236db23dd821a311
Daniil Aleshechkin <aleshechkin.daniil@gmail.com>
2023-07-25 08:13:20 -0600

    Added unit tests

--- src/test/java/com/pss/handlers/GetProjectileDragTest.java
+++ src/test/java/com/pss/handlers/GetProjectileDragTest.java
@@ -16,6 +16,7 @@ import com.pss.utils.TestUtilities;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -88,6 +89,7 @@ public class GetProjectileDragTest {
 
         GetProjectileDrag getProjectileDrag = new GetProjectileDrag(configurationHandler, projectileHandler);
         Vector3d result = getProjectileDrag.getProjectileDrag();
+        System.out.println(result);
 
         TestUtilities.assertEqualsVector(expected, result, 0.0001);
     }
@@ -142,7 +144,46 @@ public class GetProjectileDragTest {
                 // Test zero/negative Vector input case in getProjectileDrag.java
                 Arguments.of("Path Coverage : Zero Velocity", new Vector3d(0, 0, 0), 1, 1, 1, new Vector3d(0, 0, 0)),
                 // Test zero/negative dragMagnitude calculation case in getProjectileDrag.java
-                Arguments.of("Path Coverage : Zero dragMagnitude", new Vector3d(1, 1, 1), -1, 1, 1, new Vector3d(0, 0, 0)));
+                Arguments.of("Path Coverage : Zero dragMagnitude", new Vector3d(1, 1, 1), -1, 1, 1, new Vector3d(0, 0, 0)),
+
+                // Boundary value cases
+                Arguments.of("Boundary value testing : Max z velocity", new Vector3d(230, 230, 900), 1.2, 0.018, 0.3, new Vector3d(-713.13732, -713.13732, -2790.53734)),
+                Arguments.of("Boundary value testing : Max x velocity", new Vector3d(900, 230, 230), 1.2, 0.018, 0.3, new Vector3d(-2790.5373, -713.13732, -713.13732)),
+                Arguments.of("Boundary value testing : Max y velocity", new Vector3d(230, 900, 230), 1.2, 0.018, 0.3, new Vector3d(-713.13732, -2790.53734, -713.13732)),
+                Arguments.of("Boundary value testing : Min y velocity", new Vector3d(230, 0, 230), 1.2, 0.018, 0.3, new Vector3d(-242.39054, 0, -242.39054)),
+                Arguments.of("Boundary value testing : Min z velocity", new Vector3d(230, 230, 0), 1.2, 0.018, 0.3, new Vector3d(-242.39054, -242.39054, 0)),
+                Arguments.of("Boundary value testing : Min x velocity", new Vector3d(0, 230, 230), 1.2, 0.018, 0.3, new Vector3d(0, -242.39054, -242.39054)),
+                Arguments.of("Boundary value testing : Max density", new Vector3d(230, 230, 230), 1.5, 0.018, 0.3, new Vector3d(-371.08322, -371.08322, -371.08322)),
+                Arguments.of("Boundary value testing : Min density", new Vector3d(230, 230, 230), 1, 0.018, 0.3, new Vector3d(-247.38881, -247.38881, -247.38881)),
+                Arguments.of("Boundary value testing : Max area", new Vector3d(230, 230, 230), 1.2, 0.14, 0.3, new Vector3d(-2308.96229, -2308.96229, -2308.96229)),
+                Arguments.of("Boundary value testing : Min area", new Vector3d(230, 230, 230), 1.2, 0.0086, 0.3, new Vector3d(-141.83625, -141.83625, -141.83625)),
+                Arguments.of("Boundary value testing : Max drag co", new Vector3d(230, 230, 230), 1.2, 0.018, 0.6, new Vector3d(-593.73316, -593.73316, -593.73316)),
+                Arguments.of("Boundary value testing : Min drag co", new Vector3d(230, 230, 230), 1.2, 0.018, 0.1, new Vector3d(-98.95552, -98.95552, -98.95552)),
+                Arguments.of("Boundary value testing : Nominal", new Vector3d(230, 230, 230), 1.2, 0.018, 0.6, new Vector3d(-593.73316, -593.73316, -593.73316)),
+                
+                // Equivalence class cases
+                Arguments.of("Equivalence class testing : Very fast x", new Vector3d(700, 200, 200), 1.2, 0.019, 0.2, new Vector3d(-1204.95357, -344.27245, -344.27245)),
+                Arguments.of("Equivalence class testing : Fast x", new Vector3d(400, 200, 200), 1.2, 0.019, 0.2, new Vector3d(-446.78692, -223.39346, -223.39346)),
+                Arguments.of("Equivalence class testing : Slow x", new Vector3d(200, 200, 200), 1.2, 0.019, 0.2, new Vector3d(-157.96303, -157.96303, -157.96303)),
+                Arguments.of("Equivalence class testing : Stationairy x", new Vector3d(0, 200, 200), 1.2, 0.019, 0.2, new Vector3d(0, -128.97627, -128.97627)),
+                Arguments.of("Equivalence class testing : Very fast y", new Vector3d(200, 700, 200), 1.2, 0.019, 0.2, new Vector3d(-344.27245, -1204.95357, -344.27245)),
+                Arguments.of("Equivalence class testing : Fast y", new Vector3d(200, 400, 200), 1.2, 0.019, 0.2, new Vector3d(-223.39346, -446.78692, -223.39346)),
+                Arguments.of("Equivalence class testing : Slow y", new Vector3d(200, 200, 200), 1.2, 0.019, 0.2, new Vector3d(-157.96303, -157.96303, -157.96303)),
+                Arguments.of("Equivalence class testing : Stationairy y", new Vector3d(200, 0, 200), 1.2, 0.019, 0.2, new Vector3d(-128.97627, 0, -128.97627)),
+                Arguments.of("Equivalence class testing : Very fast z", new Vector3d(200, 200, 700), 1.2, 0.019, 0.2, new Vector3d(-344.27245, -344.27245, -1204.95357)),
+                Arguments.of("Equivalence class testing : Fast z", new Vector3d(200, 200, 400), 1.2, 0.019, 0.2, new Vector3d(-223.39346, -223.39346, -446.78692)),
+                Arguments.of("Equivalence class testing : Slow z", new Vector3d(200, 200, 200), 1.2, 0.019, 0.2, new Vector3d(-157.96303, -157.96303, -157.96303)),
+                Arguments.of("Equivalence class testing : Stationairy z", new Vector3d(200, 200, 0), 1.2, 0.019, 0.2, new Vector3d(-128.97627, -128.97627, 0)),
+                Arguments.of("Equivalence class testing : Cold Temp", new Vector3d(200, 200, 200), 1.3, 0.019, 0.2, new Vector3d(-171.12661, -171.12661, -171.12661)),
+                Arguments.of("Equivalence class testing : Normal Temp", new Vector3d(200, 200, 200), 1.2, 0.019, 0.2, new Vector3d(-157.96303, -157.96303, -157.96303)),
+                Arguments.of("Equivalence class testing : Hot Temp", new Vector3d(200, 200, 200), 1.1, 0.019, 0.2, new Vector3d(-144.79944, -144.79944, -144.79944)),
+                Arguments.of("Equivalence class testing : Average Cd", new Vector3d(230, 230, 230), 1.2, 0.019, 0.2, new Vector3d(-208.90611, -208.90611, -208.90611)),
+                Arguments.of("Equivalence class testing : Ok Cd", new Vector3d(200, 200, 200), 1.2, 0.019, 0.4, new Vector3d(-315.92606, -315.92606, -315.92606)),
+                Arguments.of("Equivalence class testing : Bad Cd", new Vector3d(200, 200, 200), 1.2, 0.019, 0.6, new Vector3d(-473.88910, -473.88910, -473.88910)),
+                Arguments.of("Equivalence class testing : Small area", new Vector3d(200, 200, 200), 1.2, 0.0086, 0.2, new Vector3d(-71.49905, -71.49905, -71.49905)),
+                Arguments.of("Equivalence class testing : Medium area", new Vector3d(200, 200, 200), 1.2, 0.019, 0.2, new Vector3d(-157.96303, -157.96303, -157.96303)),
+                Arguments.of("Equivalence class testing : Large area", new Vector3d(200, 200, 200), 1.2, 0.045, 0.2, new Vector3d(-374.12297, -374.12297, -374.12297))
+            );    
     }
 
 }
